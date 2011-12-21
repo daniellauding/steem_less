47feb3bb66fd11c2e6047dee86708bca974cc0fb
Daniel Lauding <daniel@lauding.se>
2011-12-21 16:28:45 +0100

    asdf

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -2,20 +2,36 @@ $(document).includeReady(function () {
 
     /* TOP NAV */
 
-    var ul = $('.nav, .breadcrumb, .tabs');
+    var horizontal_ul = $('.nav, .breadcrumb, .tabs');
+    var vertical_ul = $('.vertical-nav');
+	
 
-    if (ul.length) {
-        ul.children('li').hover(function() {
+    if (horizontal_ul.length) {
+        horizontal_ul.children('li').hover(function() {
             $(this).children('ul').show();
         }, function() {
             $(this).children('ul').hide();
         }).children('ul').hide();
     }
 
+    if (vertical_ul.length) {
+        vertical_ul.find('li a').toggle(function() {
+			$(this).parent().addClass('expanded');
+            $(this).parent().children('ul').show();
+		return false;
+        }, function() {
+			$(this).parent().removeClass('expanded');
+            $(this).parent().children('ul').hide();
+        }).parent().children('ul').hide();
+    }
+
+	
     $(".client-nav [rel=twipsy]").twipsy({
       live: true,
       delayOut: 0,
 	  placement: 'below'
     });
+
+
 
 });
